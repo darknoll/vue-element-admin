a718debf006acfefebb27fedb4e42b4c0f8856ab
darknoll <darknoll@users.noreply.github.com>
2019-08-26 17:46:40 +0800

    axios拦截器正确情况下返回有问题

--- src/utils/request.js
+++ src/utils/request.js
@@ -68,7 +68,7 @@ service.interceptors.response.use(
       }
       return Promise.reject(new Error(res.message || 'Error'))
     } else {
-      return res
+      return response
     }
   },
   error => {
